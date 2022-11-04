a2204c7cba70d829fbfd7917b47d7d7a97111738
mgabim <101949352+mgabim@users.noreply.github.com>
2022-11-04 11:17:12 -0300

    Update ddd.js

--- ddd.js
+++ ddd.js
@@ -1,20 +1,36 @@
-document.querySelector("#ddd").addEventListener("change", buscaAPI);
-municipios = new Array();
-function buscaAPI(evento){fetch(`https://brasilapi.com.br/api/ddd/v1/${evento.target.value}`)
-	.then(res => res.json())
-	.then(dados => {
-		municipios = dados.cities;
-		cidades();
-	});
-}
+const ul = document.querySelector("ul");
+let data = new Array();
 
+const setResponse = (name) => {
+   const li = document.createElement("li");
+   li.innerText = name;
+   ul.appendChild(li);
+};
 
-function cidades(){
-	lista = document.querySelector('ul');
-	lista.textContent = '';
-	municipios.forEach(cidade =>{
-		item = document.createElement('li');
-		item.textContent = cidade;
-		lista.append(item);
-	});
-}
+const fetchAPI = (inputValue) => {
+   ul.innerHTML = "";
+   fetch(`https://brasilapi.com.br/api/ddd/v1/${inputValue}`)
+      .then(response => response.json())
+      .then(dados => {
+         if (dados.cities) {
+            dados.cities.forEach(e => {
+               setResponse(e);
+            })
+         }
+      });
+};
+
+const h1 = (e) => {
+   const h1 = document.querySelector("h1");
+   h1.innerText = "Cidades com o DDD " + e;
+};
+
+document.getElementById('ddd').addEventListener("change", (e) => {
+   e.preventDefault()
+   fetchAPI(e.target.value);
+   h1(e.target.value)
+   e.target.value = "";
+});
+
+document.addEventListener("submit", 
+(e) => {e.preventDefault()})
